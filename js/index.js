81a37287d3730e26a9ab89334dbf8b2f8baaddce
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-05-31 21:35:16 +0500

    finished

---
+++ js/index.js
@@ -0,0 +1,123 @@
+//  1 misol
+
+// let a = +prompt('son krting');
+// switch (a) {
+//   case 1:
+//   case 2:
+//   case 12:
+//     console.log("qish");
+//     break;
+
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log("bahor");
+//     break;
+
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log("yoz");
+//     break;
+
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log("kuz");
+//     break;
+
+// default:
+// console.log('bundea oy mavchut emas');
+//     break;
+// }
+
+// 2 misol
+
+// function ball() {
+//   let son = +prompt("son krting");
+
+//   switch (true) {
+//     case son >= 90 && son <= 100:
+//       console.log('a');
+//       break;
+//     case son >= 80 && son < 90:
+//       console.log('b');
+//       break;
+//     case son >= 70 && son < 80:
+//       console.log('c');
+//       break;
+//     case son >= 60 && son < 70:
+//       console.log('d');
+//       break;
+//     case son >= 0 && son < 60:
+//       console.log('f');
+//       break;
+
+//     default:
+//       console.log("0 va 100 sonlar orasidegi sonlar krting");
+//       break;
+//   }
+// }
+//     ball();
+
+// 3 misol
+
+// function soat() {
+//   let soat = +prompt("soat krting");
+//   switch (true) {
+//     case soat >= 5 && soat <= 12:
+//       console.log("hayrli tong");
+//       break;
+//     case soat >= 12 && soat < 17:
+//       console.log("hayrli kun");
+//       break;
+//     case soat >= 17 && soat < 21:
+//       console.log("hayrli kech");
+//       break;
+//     case soat >= 21 && soat <= 24:
+//       console.log("hayrli tun");
+//       break;
+
+//     default:
+//         console.log('soat larni krting ');
+//       break;
+//   }
+// }
+//     soat();
+
+// while , do....while mislo
+
+// 4 misol
+
+// 5 misol
+
+// function juftsonlarchiqarilsin(a) {
+//  let i = 0;
+
+//     while (i <= 20) {
+//         if(i % 2 == 0){
+//          console.log(i);
+
+//         }
+//         i+=2
+//     }
+
+// }
+// juftsonlarchiqarilsin();
+
+// 6 misol
+
+// let soz = prompt("stop sozni krting");
+
+// function raqam() {
+//   while (true) {
+//     if (soz.toLowerCase() == "stop") {
+//          alert(" siz stop sozni krtdingiz");
+//       break;
+//     } else {
+//       soz = prompt("stop sozni krting");
+//     }
+//   }
+// }
+
+// raqam();
